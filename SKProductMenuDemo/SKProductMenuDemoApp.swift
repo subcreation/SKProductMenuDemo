73708c9252445269ecf4b7ce0cbc9f0ba107e00d
subcreation <in8media@gmail.com>
2023-02-06 13:28:57 -0800

    Initial Commit

---
+++ SKProductMenuDemo/SKProductMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKProductMenuDemoApp.swift
+//  SKProductMenuDemo
+//
+//  Created by Nathanael Roberton on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SKProductMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
